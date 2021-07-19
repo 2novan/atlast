35da983a03c0ace490e634315149917091ef75c5
Denovan Niget <niget.denovan@gmail.com>
2021-07-19 18:37:07 +0200

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  unauthenticated do 
+    root to: 'pages#home'
+  end
+  authenticated :user do 
+    root to: 'newsfeed#show', as: :newsfeed
+  end
+  resources :artists, only: [:index, :show] do 
+    resources :my_artists, only: [:create]
+  end
+  resources :my_artists, only: [:index, :destroy]
+  resources :releases, only: [:show]
+  resources :concerts, only: [:show, :index]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
