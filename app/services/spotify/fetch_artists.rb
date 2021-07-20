8f3dd12260f261cb743bb898b8558a187616281b
Denovan Niget <niget.denovan@gmail.com>
2021-07-20 18:27:36 +0200

    generating artists

---
+++ app/services/spotify/fetch_artists.rb
@@ -0,0 +1,23 @@
+module Spotify
+  RSpotify.authenticate("99df90ccb6964e5cbe030a71e89dc1f5", "1b2dc82a4dca4d7f8b0f6026380bcede")
+
+  class FetchArtists
+
+    def initialize(user)
+      @user = user
+      @spotify_user = RSpotify::User.new('credentials' => { 'token' => User.last.token })
+    end
+
+    def call  # Spotify::FetchArtists.new(User.last).call
+      # rails c -> reload!
+      @spotify_user.top_artists.each do |artist|
+        artist = Artist.where({name: artist.name, spotify_id: artist.id, image_url: artist.images.last["url"], genre }).first_or_create
+        # Recuperer les genres et les envoyer dans la table genre puis les associer avec genre_id
+      
+          # genres each 
+          #   genre = Genre first_or_create
+          #   GenreArtist..where(genre: genre, artist: artist).first_or_create
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'artists/index'
+  get 'artists/show'
+  get 'newsfeeds/index'
   unauthenticated do 
     root to: 'pages#home'
   end
   authenticated :user do 
-    root to: 'newsfeed#show', as: :newsfeed
+    root to: 'newsfeeds#show', as: :newsfeed
   end
   resources :artists, only: [:index, :show] do 
     resources :my_artists, only: [:create]
@@ -15,6 +17,5 @@ Rails.application.routes.draw do
 
   devise_for :users,
               controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
