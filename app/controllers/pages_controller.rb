9e5d20d5322aa9b246589de683d31d087c84b799
jeanemmanuel patier <jeanemlp@gmail.com>
2021-07-21 18:45:23 +0200

    search bar is working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,13 @@ class PagesController < ApplicationController
   def components
     @artists = Artist.all
   end
+
+  def search
+    if params[:search].blank?
+      redirect_to(root_path, alert: "Empty field!") and return
+    else
+      @parameter = params[:search].downcase
+      @results = Artist.where("name ILIKE ?", "%#{@parameter}%").first(5)
+    end
+  end
 end
