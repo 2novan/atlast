38c2f1cd1ca5654542b399d9bf09e7a14c887c76
Denovan Niget <niget.denovan@gmail.com>
2021-07-21 15:55:52 +0200

    Fetching data from Spotify OK

--- app/services/spotify/fetch_artists.rb
+++ app/services/spotify/fetch_artists.rb
@@ -10,17 +10,24 @@ module Spotify
 
     def call  # Spotify::FetchArtists.new(User.last).call
       # rails c -> reload!
-      @spotify_user.top_artists(limit: 5, offset: 0, time_range: 'medium_term').each do |spotify_artist|
+      @spotify_user.top_artists(limit: 2, offset: 0, time_range: 'medium_term').each do |spotify_artist|
         artist = Artist.where({name: spotify_artist.name, spotify_id: spotify_artist.id, image_url: spotify_artist.images.last["url"] }).first_or_create
+        sleep 0.05
         FollowedArtist.where({artist: artist, user: @user}).first_or_create
         spotify_artist.genres.each do |genre|
           genre = Genre.where({name: genre}).first_or_create
           artist_genre = ArtistGenre.where({genre: genre, artist: artist}).first_or_create
         end
         spotify_artist.albums.each do |release|
-          album = Release.where({name: release.name, album_type: release.album_type, spotify_id: release.id, image_url: release.images.last["url"], total_tracks: release.total_tracks}).first_or_create
-          album.tracks.each do |track|
-            track = Track.where({name: track.name, duration: track.duration_ms, track_number: track.track_number, spotify_id: track.id})
+          sleep 0.05
+          
+          # binding.pry
+          release_date = release.release_date =~ /\d{4}-\d{2}-\d{2}/ ? release.release_date : Date.new(release.release_date.to_i, 1, 1)
+          album = Release.where({name: release.name, album_type: release.album_type, artist_id: artist, image_url: release.images.last["url"], total_tracks: release.total_tracks, artist_id: artist, release_date: release_date, spotify_id: release.id}).first_or_create
+          release.tracks.each do |track|
+            sleep 0.05
+            track = Track.where({name: track.name, duration: track.duration_ms, track_number: track.track_number, spotify_id: track.id, release: album}).first_or_create!
+            track_artist = TrackArtist.where({track_id: track, artist_id: artist}).first_or_create!
           end
         end
       end
