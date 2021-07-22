b3ab9bdac5e2e5f84a7b306873a2c9c13dd677f7
Denovan Niget <niget.denovan@gmail.com>
2021-07-22 20:18:29 +0200

    timecode on tracks

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,11 +1,7 @@
 class Track < ApplicationRecord
   belongs_to :release
 
-  # def duration
-  #   self.duration
-  # end
-
   def formatted_duration
-    Time.at(duration).utc.strftime("%S")
+    Time.at(duration).utc.strftime("%H:%S")
   end
 end
