151e66a720ef1b98de9c98114a4efe2d95f2ce72
Denovan Niget <niget.denovan@gmail.com>
2021-07-22 14:49:40 +0200

    newsfeed OK

--- app/controllers/newsfeeds_controller.rb
+++ app/controllers/newsfeeds_controller.rb
@@ -2,6 +2,12 @@ class NewsfeedsController < ApplicationController
   def show
   end
   def index
-    @artists = Artist.all
+    @releases = Release.all
+    @releases.each do |release|
+      release.tracks.each do |track|
+        @duration =+ track.duration
+      end
+    end
+    
   end
 end
