72419b9ee242db6911657f372f9b1cba3a309b4d
jeanemmanuel patier <jeanemlp@gmail.com>
2021-07-23 14:30:15 +0200

    Created welcome page

--- app/javascript/plugins/init_swiper.js
+++ app/javascript/plugins/init_swiper.js
@@ -1,21 +1,31 @@
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
+import SwiperCore, { Navigation, Pagination } from 'swiper/core';
+SwiperCore.use([Navigation, Pagination]);
 
 const initSwiper =() => {
   new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: false,
+
     allowTouchMove: true,
+
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
     spaceBetween: 30,
+
     pagination: {
       el: ".swiper-pagination",
-      dynamicBullets: true,
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
+      },
     },
+
     // And if we need scrollbar
     scrollbar: {
       el: '.swiper-scrollbar',
